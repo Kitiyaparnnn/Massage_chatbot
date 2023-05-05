07c06ffb9a82d6c3ab2bca9d00fa1d3224d714eb
Kitiyaparnnn <parn.takhum@gmail.com>
2023-05-05 22:41:04 +0700

    connect line webhook:

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-app.get('/webhook', (req, res) => res.sendStatus(200))
+app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
