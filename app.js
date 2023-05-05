a6ec66b8f5dec13f71cbb0a212b2ddca7447d0af
Kitiyaparnnn <parn.takhum@gmail.com>
2023-05-05 23:00:46 +0700

    line webhook test

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const request = require('express/lib/request')
 const app = express()
+const token = require('token')
+
 const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+
 app.listen(port)
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
