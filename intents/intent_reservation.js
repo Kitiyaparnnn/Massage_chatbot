05c28569f4070eb5cec2c2f31246b7aeb69f109c
Kitiyaporn <parn.takhum@gmail.com>
2023-09-01 01:44:00 +0700

    date picker testing1.7.9

--- intents/intent_reservation.js
+++ intents/intent_reservation.js
@@ -8,30 +8,30 @@ exports.intentReservation = function intentReservation(stage) {
   var max_date = new Date(new Date().toLocaleString({ timeZone: 'Asia/Bangkok' }));
   max_date.setDate(today_date.getDate() + 3);
 
-  // var current_date = today_date.toISOString().split('T')[0];
-  // var current_time = today_date.toLocaleString('de-DE', { hour: '2-digit', hour12: false, timeZone: 'Asia/Bangkok' }).split(' ')[0] + ":" + String(today_date.getMinutes()).padStart(2, '0');
-  // var today_format = current_date + "t" + current_time;
+  var current_date = today_date.toISOString().split('T')[0];
+  var current_time = today_date.toLocaleString('de-DE', { hour: '2-digit', hour12: false, timeZone: 'Asia/Bangkok' }).split(' ')[0] + ":" + String(today_date.getMinutes()).padStart(2, '0');
+  var today_format = current_date + "t" + current_time;
 
-  // var next3_date = max_date.toISOString().split('T')[0];
-  // var next3_time = max_date.toLocaleString('de-DE', { hour: '2-digit', hour12: false, timeZone: 'Asia/Bangkok' }).split(' ')[0] + ":" + String(max_date.getMinutes()).padStart(2, '0');
-  // var next3_format = next3_date + "t" + next3_time;
+  var next3_date = max_date.toISOString().split('T')[0];
+  var next3_time = max_date.toLocaleString('de-DE', { hour: '2-digit', hour12: false, timeZone: 'Asia/Bangkok' }).split(' ')[0] + ":" + String(max_date.getMinutes()).padStart(2, '0');
+  var next3_format = next3_date + "t" + next3_time;
 
-  function toIsoString(date) {
-    var tzo = -date.getTimezoneOffset(),
-      dif = tzo >= 0 ? '+' : '-',
-      pad = function (num) {
-        return (num < 10 ? '0' : '') + num;
-      };
+  // function toIsoString(date) {
+  //   var tzo = -date.getTimezoneOffset(),
+  //     dif = tzo >= 0 ? '+' : '-',
+  //     pad = function (num) {
+  //       return (num < 10 ? '0' : '') + num;
+  //     };
 
-    return date.getFullYear() +
-      '-' + pad(date.getMonth() + 1) +
-      '-' + pad(date.getDate()) +
-      'T' + pad(date.getHours()) +
-      ':' + pad(date.getMinutes());
+  //   return date.getFullYear() +
+  //     '-' + pad(date.getMonth() + 1) +
+  //     '-' + pad(date.getDate()) +
+  //     'T' + pad(date.getHours()) +
+  //     ':' + pad(date.getMinutes());
 
-  }
-  var today_format = toIsoString(today_date);
-  var next3_format = toIsoString(max_date);
+  // }
+  // var today_format = toIsoString(today_date);
+  // var next3_format = toIsoString(max_date);
 
   console.log(`today: ${today_format}`);
   console.log(`max_date: ${next3_format}`);
