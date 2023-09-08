385be533d1fcbc88c72f87edb398f8c1b893aea5
Kitiyaporn <parn.takhum@gmail.com>
2023-09-08 14:19:23 +0700

    debug & change design final

--- intents/intent_confirmReservation.js
+++ intents/intent_confirmReservation.js
@@ -18,176 +18,8 @@ function intentConfirmReservation(msg, detail) {
           "size": "sm",
           "text": "RECEIPT",
           "type": "text",
-          "weight": "bold"
-        },
-        {
-          "margin": "md",
-          "size": "xxl",
-          "text": "ยืนยันข้อมูลการจอง",
-          "type": "text",
-          "weight": "bold"
-        },
-        {
-          "size": "sm",
-          "text": `คุณ ${detail.fullName} `,
-          "type": "text",
-          "wrap": true,
-          "weight": "bold"
-        },
-        {
-          "size": "sm",
-          "text": `เบอร์ ${detail.phoneNo}`,
-          "type": "text",
-          "wrap": true,
-          "weight": "bold"
-        },
-        {
-          "color": "#aaaaaa",
-          "size": "xs",
-          "text": `คุณ ${detail.fullName} เบอร์ ${detail.phoneNo}`,
-          "type": "text",
-          "wrap": true
-        },
-        {
-          "margin": "md",
-          "type": "separator"
-        },
-        {
-          "contents": [
-            {
-              "contents": [
-                {
-                  "color": "#555555",
-                  "flex": 2,
-                  "size": "sm",
-                  "text": "บริการ",
-                  "type": "text",
-                  "wrap": true
-                },
-                {
-                  "align": "end",
-                  "color": "#111111",
-                  "flex": 1,
-                  "size": "sm",
-                  "text": detail.massage_plan,
-                  "type": "text"
-                }
-              ],
-              "layout": "horizontal",
-              "type": "box"
-            },
-            {
-              "contents": [
-                {
-                  "color": "#555555",
-                  "flex": 0,
-                  "size": "sm",
-                  "text": "วันที่จอง",
-                  "type": "text"
-                },
-                {
-                  "align": "end",
-                  "color": "#111111",
-                  "size": "sm",
-                  "text": format_date,
-                  "type": "text"
-                }
-              ],
-              "layout": "horizontal",
-              "type": "box"
-            },
-            {
-              "contents": [
-                {
-                  "color": "#555555",
-                  "flex": 0,
-                  "size": "sm",
-                  "text": "เวลา",
-                  "type": "text"
-                },
-                {
-                  "align": "end",
-                  "color": "#111111",
-                  "size": "sm",
-                  "text": time,
-                  "type": "text"
-                }
-              ],
-              "layout": "horizontal",
-              "type": "box"
-            },
-            {
-              "contents": [
-                {
-                  "color": "#555555",
-                  "flex": 1,
-                  "size": "sm",
-                  "text": "จำนวนชั่วโมง",
-                  "type": "text",
-                  "wrap": true
-                },
-                {
-                  "align": "end",
-                  "color": "#111111",
-                  "flex": 1,
-                  "size": "sm",
-                  "text": `${detail.duration} ชม.`,
-                  "type": "text"
-                }
-              ],
-              "layout": "horizontal",
-              "type": "box"
-            }
-          ],
-          "layout": "vertical",
-          "margin": "md",
-          "spacing": "sm",
-          "type": "box"
-        },
-        {
-          "margin": "md",
-          "type": "separator"
-        },
-        {
-          "contents": [
-            {
-              "action": {
-                "label": "ยกเลิก",
-                "text": "ยกเลิก",
-                "type": "message"
-              },
-              "type": "button"
-            },
-            {
-              "action": {
-                "label": "ยืนยัน",
-                "text": "ยืนยัน",
-                "type": "message"
-              },
-              "color": "#1DB446",
-              "type": "button"
-            }
-          ],
-          "layout": "horizontal",
-          "margin": "md",
-          "type": "box"
-        }
-      ],
-      "layout": "vertical",
-      "type": "box"
-    }
-  };
-
-  let content2 = {
-    "type": "bubble",
-    "body": {
-      "contents": [
-        {
-          "color": "#1DB446",
-          "size": "sm",
-          "text": "RECEIPT",
-          "type": "text",
-          "weight": "bold"
+          "weight": "bold",
+          "margin": "md"
         },
         {
           "margin": "md",
@@ -197,21 +29,21 @@ function intentConfirmReservation(msg, detail) {
           "weight": "bold"
         },
         {
-          "size": "sm",
-          "text": `คุณ ${detail.fullName}`,
+          "size": "md",
+          "text": `คุณ ${detail.fullName} `,
           "type": "text",
           "wrap": true,
           "weight": "bold"
         },
         {
-          "size": "sm",
+          "size": "md",
           "text": `เบอร์ ${detail.phoneNo}`,
           "type": "text",
           "wrap": true,
           "weight": "bold"
         },
         {
-          "margin": "xxl",
+          "margin": "md",
           "type": "separator"
         },
         {
@@ -231,7 +63,7 @@ function intentConfirmReservation(msg, detail) {
                   "color": "#111111",
                   "flex": 1,
                   "size": "sm",
-                  "text": detail.massage_plan,
+                  "text": detail.massagePlan,
                   "type": "text"
                 }
               ],
@@ -307,7 +139,7 @@ function intentConfirmReservation(msg, detail) {
           "type": "box"
         },
         {
-          "margin": "xxl",
+          "margin": "md",
           "type": "separator"
         },
         {
@@ -343,7 +175,7 @@ function intentConfirmReservation(msg, detail) {
   let result = {
     "type": "flex",
     "altText": "confirmation card",
-    "contents": content2
+    "contents": content
   };
 
   return result;
