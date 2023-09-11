b03f00d8316584351a6bb1b32afbfcc4e520b717
Kitiyaporn <parn.takhum@gmail.com>
2023-09-11 17:32:00 +0700

    change open-close design2

--- intents/intent_openClose.js
+++ intents/intent_openClose.js
@@ -123,7 +123,7 @@ async function intentOpenClose() {
                 "type": "box"
               },
               {
-                // "backgroundColor": "#ffffff1A",
+                "backgroundColor": "#ffffff1A",
                 "contents": [
                   {
                     "contents": [
